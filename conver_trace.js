185bbaa58f3acac231c6c10f105afc7d04fc7d5b
dengshijie <736738213@qq.com>
2018-10-19 18:25:47 +0800

    鼠标移动轨迹转换成字符串

---
+++ conver_trace.js
@@ -0,0 +1,46 @@
+function pi(a, b) {
+    return (Array(b).join(0) + a).slice(-b)
+};
+
+function st(d) {
+    var c = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-~".split("")
+      , b = c.length
+      , e = +d
+      , a = [];
+    do {
+        mod = e % b;
+        e = (e - mod) / b;
+        a.unshift(c[mod])
+    } while (e);return a.join("")
+};
+
+function pm(d, c, b) {
+    var f = this;
+    var e = st(Math.abs(d));
+    var a = "";
+    if (!b) {
+        a += (d > 0 ? "1" : "0")
+    }
+    a += pi(e, c);
+    return a
+};
+
+function gc(c) {
+    var g = this;
+    var b = new Array();
+    for (var e = 0; e < c.length; e++) {
+        if (e == 0) {
+            b.push(pm(c[e][0] < 262143 ? c[e][0] : 262143, 3, true));
+            b.push(pm(c[e][1] < 16777215 ? c[e][1] : 16777215, 4, true));
+            b.push(pm(c[e][2] < 4398046511103 ? c[e][2] : 4398046511103, 7, true))
+        } else {
+            var a = c[e][0] - c[e - 1][0];
+            var f = c[e][1] - c[e - 1][1];
+            var d = c[e][2] - c[e - 1][2];
+            b.push(pm(a < 4095 ? a : 4095, 2, false));
+            b.push(pm(f < 4095 ? f : 4095, 2, false));
+            b.push(pm(d < 16777215 ? d : 16777215, 4, true))
+        }
+    }
+    return b.join("")
+}
